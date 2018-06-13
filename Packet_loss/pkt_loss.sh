b546751bcd33d26e4daca11b43102afbcb141989
vasanth889 <38485903+vasanth889@users.noreply.github.com>
2018-06-13 17:19:02 +0530

    Update pkt_loss.sh
    deleted the packet loss delete part

--- Packet_loss/pkt_loss.sh
+++ Packet_loss/pkt_loss.sh
@@ -9,12 +9,4 @@ sleep 5
 echo "show active rules"
 tc qdisc show  dev wlp4s0
 
-sleep 5
 
-echo "delete all rules"
-sudo tc qdisc del dev wlp4s0 root
-
-sleep 5
-
-echo "show rules"
-tc qdisc show  dev wlp4s0
